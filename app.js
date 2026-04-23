001f61c2b3cd06032219a18b72b691dfe8097de4
Meepster27 <sharpieisme27@gmail.com>
2026-04-23 18:10:41 -0500

    swipe scroll

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
   return (
-    <>
+    <GestureHandlerRootView style={styles.root}>
       <AppNavigator />
       <StatusBar style="light" />
-    </>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  root: { flex: 1 },
+});
