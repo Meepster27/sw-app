d4f9b014eb0175d91d47cae36986789477dd6296
Meepster27 <sharpieisme27@gmail.com>
2026-04-30 17:55:13 -0500

    web

--- screens/FilmsScreen.js
+++ screens/FilmsScreen.js
@@ -109,9 +109,9 @@ export default function FilmsScreen({ navigation }) {
         renderRightActions={renderRightAction}
         onSwipeableOpen={() => handleSwipe(item)}
       >
-        <View>
+        <TouchableOpacity activeOpacity={0.8} onPress={() => handleSwipe(item)}>
           {renderCardContent(item)}
-        </View>
+        </TouchableOpacity>
       </Swipeable>
     );
   };
